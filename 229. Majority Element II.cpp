60b71657b03abeb52eea0a8c6f56bd92c7c3b0e3
Shashank-dev-i <shashankchourasia1211@gmail.com>
2026-03-23 23:05:30 +0530

    229. Majority Element II(Optimal)

---
+++ 229. Majority Element II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int, int> m;
+        vector<int> ans;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+        }
+
+        for (auto x : m) {
+            if (x.second > n / 3) {
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
