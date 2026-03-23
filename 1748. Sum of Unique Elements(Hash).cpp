76a4af4078055107bed0358827a324f41ac73e44
Shashank-dev-i <shashankchourasia1211@gmail.com>
2026-03-23 23:56:53 +0530

    1748. Sum of Unique Elements(Hash)(Optimal)

---
+++ 1748. Sum of Unique Elements(Hash).cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> m;
+        int sum = 0;
+
+        for(int i = 0 ; i<n ; i++){
+            m[nums[i]]++;
+        }
+
+        for(auto x:m){
+            if(x.second ==1){
+                sum = sum + x.first;
+            }
+        }
+        return sum;
+    }
+};class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> m;
+        int sum = 0;
+
+        for(int i = 0 ; i<n ; i++){
+            m[nums[i]]++;
+        }
+
+        for(auto x:m){
+            if(x.second ==1){
+                sum = sum + x.first;
+            }
+        }
+        return sum;
+    }
+};
