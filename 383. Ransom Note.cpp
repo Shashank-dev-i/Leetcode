95f49f482196ff4eba97650b00c4488cda0d7742
Shashank-dev-i <shashankchourasia1211@gmail.com>
2026-03-28 20:02:20 +0530

    383. Ransom Note(Optimal)

---
+++ 383. Ransom Note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        int freq[26] = {0};
+        for (auto c : ransomNote) {
+            freq[c - 'a']++;
+        }
+
+        for (auto c : magazine) {
+            freq[c - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
